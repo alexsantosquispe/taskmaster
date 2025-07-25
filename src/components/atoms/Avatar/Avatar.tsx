1f09595c7434cbed2a36ffe912a34ad27dc29e63
Alex Santos <alexsantosquispe@gmail.com>
2025-07-25 16:51:48 -0400

    SEO issue fixed

--- src/components/atoms/Avatar/Avatar.tsx
+++ src/components/atoms/Avatar/Avatar.tsx
@@ -7,7 +7,7 @@ export const Avatar = ({ url }: AvatarProps) => {
   return (
     <div className="flex w-fit items-center justify-center overflow-hidden rounded-full bg-gray-200 p-0.5 text-neutral-500">
       {url ? (
-        <img src={url} className="h-5 w-5" />
+        <img src={url} className="h-5 w-5" alt="User avatar image" />
       ) : (
         <UserIcon className="size-5" />
       )}
