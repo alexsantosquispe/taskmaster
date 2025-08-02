732f785cf1ce66acd291bf35ac0c8d1c651846de
Alex Santos <alexsantosquispe@gmail.com>
2025-08-02 18:57:34 -0400

    storybook for Breadcrumb

---
+++ src/components/atoms/Breadcrumb/Breadcrumb.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Breadcrumb } from './Breadcrumb';
+
+const BREADCRUMBS = [
+  {
+    id: 'projects',
+    label: 'Projects',
+    href: ''
+  },
+  {
+    id: 'proj-001',
+    label: 'TaskMaster Mobile App',
+    href: ''
+  },
+  {
+    id: 'task-1',
+    label: 'Task-001',
+    href: ''
+  },
+  {
+    id: 'sub-task-5',
+    label: 'SubTask-005',
+    href: ''
+  }
+];
+
+const meta = {
+  title: 'Atoms/Breadcrumb',
+  component: Breadcrumb,
+  parameters: {
+    layout: 'centered'
+  }
+} satisfies Meta<typeof Breadcrumb>;
+
+export default meta;
+
+type Story = StoryObj<typeof Breadcrumb>;
+
+export const Default: Story = () => {
+  return <Breadcrumb items={BREADCRUMBS} />;
+};
+
+Default.args = {};
