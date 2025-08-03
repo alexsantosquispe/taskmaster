230315ba288953243a77475ae5c43d0f52740e38
Alex Santos <alexsantosquispe@gmail.com>
2025-08-02 20:30:07 -0400

    TaskItemCard story added

---
+++ src/components/atoms/TaskItem/TaskItemCard.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { DONE_TASKS, IN_PROGRESS_TASKS } from '../../../utils/data/mocks';
+import { TaskItemCard } from './TaskItemCard';
+
+const inProgressCard = IN_PROGRESS_TASKS[0];
+const doneCard = DONE_TASKS[1];
+
+const meta = {
+  title: 'Atoms/TaskItemCard',
+  component: TaskItemCard,
+  parameters: {
+    layout: 'centered'
+  }
+} satisfies Meta<typeof TaskItemCard>;
+
+export default meta;
+
+type Story = StoryObj<typeof TaskItemCard>;
+
+export const Default: Story = () => {
+  return (
+    <div className="flex w-2/3 gap-8 self-center">
+      <TaskItemCard {...inProgressCard} />
+      <TaskItemCard {...doneCard} />
+    </div>
+  );
+};
+
+Default.args = {};
