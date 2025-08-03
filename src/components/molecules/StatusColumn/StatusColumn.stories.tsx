1a7bf67cb2929114b62296069a40dc0b10a489b4
Alex Santos <alexsantosquispe@gmail.com>
2025-08-03 13:26:35 -0400

    StatusColumn story added

---
+++ src/components/molecules/StatusColumn/StatusColumn.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { CircleDotDashedIcon } from '../../../icons';
+import { BACKLOG_TASKS } from '../../../utils/data/mocks';
+import { StatusColumn } from './StatusColumn';
+
+const meta = {
+  title: 'Molecules/StatusColumn',
+  component: StatusColumn
+} satisfies Meta<typeof StatusColumn>;
+
+export default meta;
+
+type Story = StoryObj<typeof StatusColumn>;
+
+export const Default: Story = () => {
+  return (
+    <StatusColumn
+      title="Backlog"
+      icon={<CircleDotDashedIcon />}
+      tasks={BACKLOG_TASKS}
+    />
+  );
+};
+
+Default.args = {};
