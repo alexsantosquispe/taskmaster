4838b4ad8062da92db4039a5b4eee70691e90657
Alex Santos <alexsantosquispe@gmail.com>
2025-08-06 22:15:51 -0400

    detail component snapshot

---
+++ src/components/organisms/Detail/Detail.test.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+import { Detail } from './Detail';
+
+describe('Detail component', () => {
+  it('should render the component correctly', () => {
+    const component = render(<Detail />);
+
+    expect(component).toMatchSnapshot();
+  });
+});
