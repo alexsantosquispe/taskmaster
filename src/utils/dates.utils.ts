8c5f7edc8dd743feda7eafc931e87f2b18ab82fd
Alex Santos <alexsantosquispe@gmail.com>
2025-08-20 23:51:33 -0400

    function removed

--- src/utils/dates.utils.ts
+++ src/utils/dates.utils.ts
@@ -8,25 +8,6 @@ export const formatDateToDayMonth = (date: string) => {
   return new Intl.DateTimeFormat('en-US', options).format(new Date(date));
 };
 
-export const formatStringDateToUTCDate = (date: string): Date | null => {
-  const parsedDate = new Date(date);
-
-  if (isNaN(parsedDate.getTime())) return null;
-
-  // Create a new Date using UTC components to avoid timezone offset issues
-  const formattedDate = new Date(
-    parsedDate.getUTCFullYear(),
-    parsedDate.getUTCMonth(),
-    parsedDate.getUTCDate(),
-    parsedDate.getUTCHours(),
-    parsedDate.getUTCMinutes(),
-    parsedDate.getUTCSeconds(),
-    parsedDate.getUTCMilliseconds()
-  );
-
-  return formattedDate;
-};
-
 export const formatToFriendlyDate = (date: string): string => {
   const parsedDate = new Date(date);
 
