f17fbfbb0f8411c88069a306a539ed44aef44eaa
Alex Santos <alexsantosquispe@gmail.com>
2025-08-22 10:32:32 -0400

    TextAreaField story added

---
+++ src/components/atoms/TextAreaField/TextAreaField.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Wrapper } from '@/utils/testing/unitTest.util';
+import { useForm } from 'react-hook-form';
+import { TextAreaField } from './TextAreaField';
+
+const meta = {
+  title: 'Atoms/TextAreaField',
+  component: TextAreaField,
+  parameters: {
+    layout: 'centered'
+  }
+} satisfies Meta<typeof TextAreaField>;
+
+export default meta;
+
+type Story = StoryObj<typeof TextAreaField>;
+
+export const Default: Story = () => {
+  const { control } = useForm();
+  const props = {
+    label: 'Detail',
+    name: 'detail',
+    placeholder: 'Enter text...'
+  };
+
+  return (
+    <div className="flex gap-4">
+      <Wrapper>
+        <TextAreaField {...props} control={control} />
+      </Wrapper>
+      <Wrapper>
+        <TextAreaField {...props} isDisabled={true} control={control} />
+      </Wrapper>
+      <Wrapper>
+        <TextAreaField {...props} isRequired={true} control={control} />
+      </Wrapper>
+      <Wrapper>
+        <TextAreaField
+          {...props}
+          errorMessage="Error message"
+          control={control}
+        />
+      </Wrapper>
+    </div>
+  );
+};
+
+Default.args = {};
