f00092581638e66f284a1628bec972e4384c7da2
Alex Santos <alexsantosquispe@gmail.com>
2025-09-16 19:34:11 -0400

    logout button

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,8 +1,13 @@
 import { Avatar } from '@/components/atoms/Avatar/Avatar';
+import { Button } from '@/components/atoms/Button/Button';
 import { ProfileForm } from '@/components/molecules/ProfileForm/ProfileForm';
 import type { ProfileFormType } from '@/components/molecules/ProfileForm/ProfileForm.schema';
+import { useSignOutMutation } from '@/services/authApi';
+import cn from 'clsx';
 
 const Profile = () => {
+  const [signOut, signOutResult] = useSignOutMutation();
+
   const onSubmitProfile = (formData: ProfileFormType) => {
     //TODO: This console should be removed when the logic to update the profile is implemented
     console.log(formData);
@@ -31,6 +36,26 @@ const Profile = () => {
 
           <ProfileForm onSubmitProfile={onSubmitProfile} />
         </div>
+      </article>
+
+      <article className="dark:bg-primary flex items-center justify-between rounded-lg border border-neutral-200 p-4 md:px-6 dark:border-white/20">
+        <div className="flex flex-col">
+          <h3 className="text-lg font-semibold md:text-xl">Log out</h3>
+          <p className="text-sm text-neutral-600 dark:text-white/70">
+            Close your session here.
+          </p>
+        </div>
+
+        <Button
+          label="Log out"
+          ariaLabel="Log out"
+          className={cn({
+            'bg-rose-700 hover:bg-rose-600 dark:bg-rose-800 dark:hover:bg-rose-700':
+              !signOutResult.isLoading
+          })}
+          onClick={signOut}
+          isLoading={signOutResult.isLoading}
+        />
       </article>
     </section>
   );
