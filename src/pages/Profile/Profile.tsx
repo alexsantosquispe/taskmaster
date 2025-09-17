7bb3ded92eee65295a0e151ced957db164a46c40
Alex Santos <alexsantosquispe@gmail.com>
2025-09-16 22:25:26 -0400

    profile styles

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -14,7 +14,7 @@ const Profile = () => {
   };
 
   return (
-    <section className="flex w-full flex-col gap-8 px-4 md:px-0 xl:max-w-[var(--width-medium-screen)]">
+    <section className="flex w-full flex-col gap-4 px-4 md:px-0 xl:max-w-[var(--width-medium-screen)]">
       <div className="flex flex-col">
         <h2 className="text-2xl font-bold md:text-3xl">Profile</h2>
         <p className="text-sm text-neutral-600 dark:text-white/70">
