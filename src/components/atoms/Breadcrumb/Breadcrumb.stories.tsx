1190ee667a1945fef5030e9d10ec53417725b0bf
Alex Santos <alexsantosquispe@gmail.com>
2026-01-01 21:01:30 -0400

    storie issue fix

--- src/components/atoms/Breadcrumb/Breadcrumb.stories.tsx
+++ src/components/atoms/Breadcrumb/Breadcrumb.stories.tsx
@@ -1,4 +1,4 @@
-import { WrapperUI } from '@/utils/test.utils';
+import { TestWrapper, WrapperUI } from '@/utils/test.utils';
 import type { Meta, StoryObj } from '@storybook/react';
 import { Breadcrumb } from './Breadcrumb';
 
@@ -40,7 +40,9 @@ type Story = StoryObj<typeof Breadcrumb>;
 export const Default: Story = () => {
   return (
     <WrapperUI>
-      <Breadcrumb items={BREADCRUMBS} />
+      <TestWrapper>
+        <Breadcrumb items={BREADCRUMBS} />
+      </TestWrapper>
     </WrapperUI>
   );
 };
